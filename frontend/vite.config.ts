631a3e4f72a9b36aef98a09ade0d0eddbc29af93
j0vanni <solanojovanni@gmail.com>
2025-03-02 12:28:34 -0500

    Remove environment variable definition from Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,13 +15,4 @@ export default defineConfig({
   build: {
     outDir: "build",
   },
-  define: {
-    __ENV__: JSON.stringify({
-      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-      GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-      JWT_SECRET: process.env.JWT_SECRET,
-      SESSION_SECRET: process.env.SESSION_SECRET,
-      TMDB_API_KEY: process.env.TMDB_API_KEY,
-    }),
-  },
 });
