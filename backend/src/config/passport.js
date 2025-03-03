6095d53858032337102233116ac3918fbcab0a08
j0vanni <solanojovanni@gmail.com>
2025-03-03 15:05:54 -0500

    Update Google OAuth callback URL to use environment variable

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -8,7 +8,7 @@ module.exports = (passport) => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback",
+        callbackURL: `${process.env.URL}/auth/google/callback`,
         scope: ["profile", "email"],
       },
       async (accessToken, refreshToken, profile, done) => {
