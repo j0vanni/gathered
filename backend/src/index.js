d248bb382ed9f4475a179f553fec3755c86e658b
j0vanni <solanojovanni@gmail.com>
2025-03-03 19:02:36 -0500

    Configure server to listen on all network interfaces

--- backend/src/index.js
+++ backend/src/index.js
@@ -64,7 +64,7 @@ app.get("/profile", (req, res) => {
   res.json(req.user);
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
 
